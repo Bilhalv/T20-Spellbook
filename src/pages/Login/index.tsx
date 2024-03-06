3c1eb8c60e27eb9747d8d3b5cb86b2b94e69f559
Pedro Bilhalva Oliveira <128099321+Bilhalv@users.noreply.github.com>
2024-03-06 20:19:12 +0000

    style: removed template text

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -7,7 +7,6 @@ const Entrar = () => {
   const [isCreatingNew, setIsCreatingNew] = useState(false);
   return (
     <>
-      <h1>here is where ill put the login form and the register form</h1>
       <div className="flex flex-col w-1/2 mx-auto mt-4">
         {isCreatingNew ? <Register /> : <Login />}
         <Button onClick={() => setIsCreatingNew(!isCreatingNew)}>
