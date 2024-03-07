d01d7794dc6d54494cb4fb54d5412e7759598afb
Bilhalv Casa <pedrokabilhalva@gmail.com>
2024-03-07 14:24:04 -0300

    Refactor Nav component and update CSS styles

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,20 +1,14 @@
 import React from "react";
-<<<<<<< Updated upstream
-=======
 import { Link } from "react-router-dom";
->>>>>>> Stashed changes
 
 export function Nav() {
     
   return (
     <nav className="bg-gray-800 p-4 text-white flex justify-between">
-<<<<<<< Updated upstream
       <h1 className="text-2xl">Grimório T20!!</h1>
-=======
-        <Link to="/" className="text-xl">Home</Link>
-        <Link to="/login" className="text-xl">Login</Link>
-        <Link to="/grimorio" className="text-xl">Grimorio</Link>
->>>>>>> Stashed changes
+      <Link to="/" className="text-xl">Home</Link>
+      <Link to="/login" className="text-xl">Login</Link>
+      <Link to="/grimorio" className="text-xl">Grimorio</Link>
     </nav>
   );
 }
