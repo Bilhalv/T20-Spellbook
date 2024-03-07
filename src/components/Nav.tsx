22ce242e52dc06641d3e40ebbd72e4fe77097715
Pedro Bilhalva Oliveira <128099321+Bilhalv@users.noreply.github.com>
2024-03-07 15:20:07 +0000

    Add new CSS classes for layout and styling

---
+++ src/components/Nav.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function Nav() {
+    
+  return (
+    <nav className="bg-gray-800 p-4 text-white flex justify-between">
+      <h1 className="text-2xl">Grimório T20!!</h1>
+    </nav>
+  );
+}
