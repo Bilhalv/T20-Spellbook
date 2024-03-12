cd068c49491f69590f7d03928d3c8de31db08324
Pedro Bilhalva Oliveira <128099321+Bilhalv@users.noreply.github.com>
2024-03-12 18:25:18 +0000

    🎨 styling

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,27 +1,43 @@
 import { Avatar, Popover } from "@mui/material";
 import firebase from "../../firebase";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { AvatarPopover } from "./AvatarPopover";
 
 export function Nav() {
+  const [janela, setJanela] = React.useState(window.location.pathname);
+  React.useEffect(() => {
+    setJanela(window.location.pathname);
+  }, [window.location.pathname]);
   return (
-    <nav className="bg-gray-800 p-4 text-white flex justify-between">
-      <Link to="/" className="text-xl">
-        Home
-      </Link>
+    <nav className="bg-gray-800 p-4 text-white flex justify-between h-20">
+      <div className="text-xl w-full flex">
+        <Link to="/" className={"transition-all " + (janela === "/" ? "border-b border-b-white w-fit" : "border-b border-b-transparent hover:border-b-white/30")}>
+          Home
+        </Link>
+      </div>
       {firebase.auth().currentUser ? (
         <>
-          <Link to="/grimorio" className="text-xl">
-            Grimorio
-          </Link>
-          <AvatarPopover />
+          <div className="text-xl w-full flex justify-center">
+            <Link to="/grimorio" className={"transition-all " + (janela === "/grimorio" ? "border-b border-b-white w-fit mx-auto" : "border-b border-b-transparent hover:border-b-white/30")}>
+              Grimorio
+            </Link>
+          </div>
+          <div className="w-full">
+            <div className="flex justify-end">
+              <AvatarPopover />
+            </div>
+          </div>
         </>
       ) : (
-        <Link to="/login" className="text-xl">
-          <button className="bg-white hover:bg-black hover:text-white text-black font-bold py-2 px-4 rounded transition-all text-sm">
-            Login
-          </button>
+        <Link to="/login" className="text-xl w-full">
+          <div className="flex justify-end">
+            <button className="bg-white hover:bg-black hover:text-white text-black py-2 px-4 rounded transition-all text-sm">
+              <p className={janela === "/login" ? "border-b border-b-white w-fit" : ""}>
+                Login
+              </p>
+            </button>
+          </div>
         </Link>
       )}
     </nav>
